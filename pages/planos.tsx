a55dd7af1ef91963e170c81babaecaa3aa2806bd
Amanda Natallie <amanda.natallie.2@gmail.com>
2020-09-08 15:15:10 -0300

    fix

--- pages/planos.tsx
+++ pages/planos.tsx
@@ -1,8 +1,5 @@
 import { Container } from "../components/Container";
 import Welcome from "./sections/Welcome";
-import Presentation from "./sections/Presentation";
-import { MuiThemeProvider } from "@material-ui/core";
-import { theme } from  "../utils/theme";
 import Plans from "./sections/Plans";
 
 export default () => {
