6e666fda87cb1048996ee4e189e72b92ff6c700a
Amanda Natallie <amanda.natallie.2@gmail.com>
2020-10-04 19:40:23 -0300

    Update Next config

--- next.config.js
+++ next.config.js
@@ -2,14 +2,13 @@ const isProd = process.env.NODE_ENV === "production";
 
 const withPWA = require("next-pwa")
 
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/
-});
 
-module.exports = withMDX(withPWA({
-  pageExtensions: ["tsx", "mdx"],
+
+module.exports = withPWA({
+  distDir: 'build',
+  pageExtensions: ["tsx", "ts"],
   pwa: {
     disable: !isProd,
     dest: "public"
   }
-}))
+})
